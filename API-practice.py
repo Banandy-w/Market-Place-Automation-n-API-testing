573fa9d70bb571287fa1c53ab0d681ca23b19393
Andy <anwong16@gmail.com>
2024-11-21 15:14:53 -0800

    added click_signup, input_signup_name and input_signup_email functions for LoginPage class

---
+++ API-practice.py
@@ -0,0 +1,134 @@
+import requests
+import json
+import jsonschema
+
+base_url = 'https://automationexercise.com/api/'
+
+auth_token = ''
+
+JSON_SCHEMA = {
+  "title": "Generated schema for Root",
+  "type": "object",
+  "properties": {
+    "responseCode": {
+      "type": "number"
+    },
+    "products": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "number"
+          },
+          "name": {
+            "type": "string"
+          },
+          "price": {
+            "type": "string"
+          },
+          "brand": {
+            "type": "string"
+          },
+          "category": {
+            "type": "object",
+            "properties": {
+              "usertype": {
+                "type": "object",
+                "properties": {
+                  "usertype": {
+                    "type": "string"
+                  }
+                },
+                "required": [
+                  "usertype"
+                ]
+              },
+              "category": {
+                "type": "string"
+              }
+            },
+            "required": [
+              "usertype",
+              "category"
+            ]
+          }
+        },
+        "required": [
+          "id",
+          "name",
+          "price",
+          "brand",
+          "category"
+        ]
+      }
+    }
+  },
+  "required": [
+    "responseCode",
+    "products"
+    "items"
+  ]
+}
+
+#GET request
+def get_request():
+    URL =  base_url + '/productsList'
+    headers = {'Authorization' : auth_token}
+    response = requests.get(URL, headers = headers)
+    
+    assert response.status_code == 200
+    status_code = response.status_code
+
+    json_data = response.json()
+    json_pretty_str = json.dumps(json_data, indent=2 )
+    #print('json response: \n', json_pretty_str)
+    
+    #validate json schema...
+    try:
+        jsonschema.validate(instance=json_data,schema=JSON_SCHEMA)
+        jsonschema_check = True
+    except jsonschema.exceptions.ValidationError as e:
+        print("JSON is invalid:", e)
+        jsonschema_check = False
+    
+    return status_code
+
+def check_schema():
+    URL =  base_url + '/productsList'
+    headers = {'Authorization' : auth_token}
+    response = requests.get(URL, headers = headers)
+    
+    assert response.status_code == 200
+    status_code = response.status_code
+
+    json_data = response.json()
+    json_pretty_str = json.dumps(json_data, indent=2 )
+    #print('json response: \n', json_pretty_str)
+    
+    #validate json schema...
+    try:
+        jsonschema.validate(instance=json_data,schema=JSON_SCHEMA)
+        jsonschema_check = True
+    except jsonschema.exceptions.ValidationError as e:
+        print("JSON is invalid:", e)
+        jsonschema_check = False
+    
+    return jsonschema_check
+
+#POST request
+
+#PUT request
+
+#DELETE request
+
+def test_GET_ProductsList():
+    assert get_request() == 200
+    assert check_schema() == True
+
+def main():
+    get_request()
+
+
+if __name__ =="__main__":
+     main()
--- POM_pages/login_page.py
+++ POM_pages/login_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from POM_pages.base_page import BasePage
 
 class LoginLocators():
     LOGIN_EMAIL_TEXTBOX = (By.CSS_SELECTOR,'.login-form > form:nth-child(2) > input:nth-child(2)')
@@ -10,8 +11,19 @@ class LoginLocators():
     LOGIN_BTN = (By.CSS_SELECTOR,'button.btn:nth-child(4)')
     SIGNUP_BTN = (By.CSS_SELECTOR,'button.btn:nth-child(5)')
 
-class LoginPage():
+class LoginPage(BasePage):
     def __init__(self,driver):
         super().__init__(driver)
+
+    def click_signup(self):
+        self.click_on(LoginLocators.SIGNUP_BTN)
+
+
+    def input_signup_name(self, username):
+        self.driver.find_element(*LoginLocators.SIGNUP_NAME_TEXTBOX).send_keys(username)
+
+    def input_signup_email(self, email):
+        self.driver.find_element(*LoginLocators.SIGNUP_EMAIL_TEXTBOX).send_keys(email)
+
 
     
